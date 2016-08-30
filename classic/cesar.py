f729e0a898e9099d1ec801a75472375b0d5eb95b
NatanJMai <natan.mai@hotmail.com>
2016-08-30 17:17:43 -0300

    Update cesar

--- classic/cesar.py
+++ classic/cesar.py
@@ -3,10 +3,18 @@ from sys import argv
 key  = int(argv[1])
 data = "So sei que nao sei"
 
-def main():
+def encrypt():
     new_d = ''
     for i in data: new_d +=  chr((ord(i) + key) % 256)
     print(new_d)
 
+def decrypt():
+    new_d = ''
+    for i in data: new_d +=  chr((ord(i) - key) % 256)
+    print(new_d)
+
 if __name__ == '__main__':
-    main()
+    if argv[2] == '-e':
+        encrypt()
+    else:
+        decrypt()
