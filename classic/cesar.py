b1bd8e00961132708bd60aadd7e49e308e0f0970
NatanJMai <natan.mai@hotmail.com>
2016-08-30 20:54:35 -0300

    Update ceasar

--- classic/cesar.py
+++ classic/cesar.py
@@ -1,20 +1,21 @@
 from sys import argv
 
+fs   = open(argv[3], 'rb')
+fd   = open(argv[4], 'wb')
 key  = int(argv[1])
-data = "So sei que nao sei"
+data = fs.read()
 
 def encrypt():
-    new_d = ''
-    for i in data: new_d +=  chr((ord(i) + key) % 256)
-    print(new_d)
-
+    fd.write(bytes((x + key) % 256 for x in data))
+    
 def decrypt():
-    new_d = ''
-    for i in data: new_d +=  chr((ord(i) - key) % 256)
-    print(new_d)
+    fd.write(bytes((x - key) % 256 for x in data))
 
 if __name__ == '__main__':
-    if argv[2] == '-e':
+    if '-h' in argv:
+        print("Usage: python3 <cesar.py> <key> <option:-e/-d> <file_input> <file_output>")
+
+    elif argv[2] == '-e':
         encrypt()
     else:
         decrypt()
