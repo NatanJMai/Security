97ec5ba5ee047336f441d097ff706ba4aadee8b7
NatanJMai <natan.mai@hotmail.com>
2016-08-30 22:06:31 -0300

    Update cesar.py

--- classic/cesar.py
+++ classic/cesar.py
@@ -4,6 +4,10 @@ fs_l = ['inputs/%s.input'  % str(x) for x in range(1, 8)]
 fd_l = ['outputs_natan/%s.output' % str(x) for x in range(1, 8)]
 key  = int(argv[1])
 
+'''
+python3 cesar.py 17 -e && for i in 1 2 3 4 5 6 7; do diff outputs/$i.input.ceasar.17 outputs_natan/$i.output; done
+'''
+
 #fs   = open(argv[3], 'rb')
 #fd   = open(argv[4], 'wb')
 
